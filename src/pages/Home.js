ad2fd1a6434ab50af9613f53b9c6b67f492b19b1
Yevhenii Sitolenko <110056243+Yevhenii770@users.noreply.github.com>
2023-05-01 18:11:59 +0200

    delete spiner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,3 @@
-import ClimbingBoxLoader from 'react-spinners/ClimbingBoxLoader';
 export default function Home() {
-  return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        position: 'relative',
-      }}
-    >
-      <div
-        style={{
-          position: 'absolute',
-          top: '158px',
-          left: '320px',
-        }}
-      >
-        <ClimbingBoxLoader color="#2196f3" size={90} />
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
