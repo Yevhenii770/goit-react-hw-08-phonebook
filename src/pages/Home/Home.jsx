4a530ab2cc1881d4c0b41718dad6b35da2f49aac
Yevhenii Sitolenko <110056243+Yevhenii770@users.noreply.github.com>
2023-11-06 14:26:07 +0100

    added home screen img

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,6 @@
+import homeImage from '../../images/phonebook.jpg';
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>{<img src={homeImage} alt="home" style={{ width: '100%' }} />}</div>
+  );
 }
